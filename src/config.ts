4f437c12cfc25bada00e3b71fe1f3885ddf753ab
Abdul Azees <abdulazeespr@gmail.com>
2025-02-05 14:45:14 +0530

    delete SECERET KEY

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const JWT_SCERET ="wqdwefwa113"
-export const MONGODB_URL ="mongodb+srv://abdulazeespr:YfmWHC1bN8BAGxyB@payme.9n8ib.mongodb.net/"
+export const JWT_SCERET =""
+export const MONGODB_URL =""
